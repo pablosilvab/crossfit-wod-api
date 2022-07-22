9475e8f78a67b3347b803a2e2b001329c3597e8d
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-07-21 20:40:37 -0400

    Using controller layer

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,24 +1,11 @@
 const express = require("express");
+const workoutController = require("../../controllers/workoutController");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Get all workouts");
-});
-
-router.get("/:workoutId", (req, res) => {
-    res.send("Get an existing workout");
-});
-
-router.post("/", (req, res) => {
-    res.send("Create a new workout");
-});
-
-router.patch("/:workoutId", (req, res) => {
-    res.send("Update an existing workout");
-});
-
-router.delete("/:workoutId", (req, res) => {
-    res.send("Delete an existing workout");
-});
+router.get("/", workoutController.getAllWorkouts);
+router.get("/:workoutId", workoutController.getOneWorkout);
+router.post("/", workoutController.createNewWorkout);
+router.patch("/:workoutId", workoutController.updateOneWorkout);
+router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
 module.exports = router;
