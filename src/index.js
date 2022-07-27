0734959b82abab9e8c577a3700308ac8325190cc
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-07-26 21:06:52 -0400

    Fix env vars

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ require('./config/config')
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-require('dotenv').config()
 
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes")
 const v1MemberRouter = require("./v1/routes/memberRoutes")
@@ -25,7 +24,7 @@ mongoose.connect(process.env.URLDB,
 );
 
 app.listen(PORT, () => {
-    console.log(`API running in ${process.env.NODE_ENV} environtment. Listening on port ${process.env.PORT}`);
+    console.log(`API running in ${process.env.NODE_ENV} environtment. Listening on port ${PORT}`);
     V1SwaggerDocs(app, PORT);
 });
 
